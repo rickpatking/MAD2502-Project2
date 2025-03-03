863c3639226deb623ce2281e4d12675fcdc592c1
rickpatking <patrickking@ufl.edu>
2025-03-03 18:17:47 -0500

    added the get_escape_time function in mandelbrot.py and group names to the project file

---
+++ mandelbrot.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def get_escape_time(c: complex, max_iterations: int) -> int | None:
+    """
+    Calculates the number of iterations that have passed before
+    the escape time of a complex number in the Mandelbrot set.
+    Uses the sequence of z_n to calculate a z_k, and when the absolute value
+    of z_k is greater than 2 the point will escape.
+
+
+    :param c: complex number to test
+    :param max_iterations: max number of iterations
+    :return: returns how many iterations have passed before the point escaped,
+    if it never escaped it will return None
+    """
+    z = c
+    for i in range(max_iterations):
+        if abs(z) > 2:
+            return i
+        z = ((z)**2) + c
+    return None
