a68c71f737b71dcff38017ff531f135a19515895
rickpatking <patrickking@ufl.edu>
2025-03-14 08:25:25 -0400

    changed get_escape_time to account for when max_iterations = 0

--- mandelbrot.py
+++ mandelbrot.py
@@ -14,10 +14,11 @@ def get_escape_time(c: complex, max_iterations: int) -> int | None:
     if it never escaped it will return None
     """
     z = c
-    for i in range(max_iterations):
-        if abs(z) > 2:
-            return i
-        z = ((z)**2) + c
+    if max_iterations != 0:
+        for i in range(max_iterations):
+            if abs(z) > 2:
+                return i
+            z = ((z)**2) + c
     if abs(z) > 2:
         return max_iterations
     return None
